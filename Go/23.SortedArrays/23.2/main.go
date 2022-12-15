c704c5c6dc16c42b05ea3e474fe2051a479f0913
Vladimir <barakovvm@ya.ru>
2022-12-15 13:59:54 +0300

    23.2 Binary search using SEARCH

--- Go/23.SortedArrays/23.2/main.go
+++ Go/23.SortedArrays/23.2/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sort"
 	"time"
 )
 
@@ -15,17 +16,32 @@ func fillMeUp() (a [size]int) {
 	}
 	return
 }
+
+func findIt(x int, a [size]int) {
+	i := sort.Search(len(a), func(i int) bool { return a[i] >= x })
+	if i < len(a) && a[i] == x {
+		fmt.Printf("found %d at index %d in %v\n", x, i, a)
+	} else {
+		fmt.Printf("%d not found in %v\n", x, a)
+	}
+}
+
 func main() {
-	fmt.Println("Задание 1. Расчёт по формуле")
+	fmt.Println("Задание 2. Нахождение первого вхождения числа в упорядоченном массиве")
 	fmt.Println("Ищу практику в интересных проектах на го. barakovvm@ya.ru ")
-	//var sortedNotUnique=[...]int{1, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	lookForMe := 2
 	whereToLook := fillMeUp()
-	for i, v := range whereToLook {
-		if v == lookForMe {
-			fmt.Println("Индекс элемента ", i)
-			break
-		}
-	}
+	fmt.Println(whereToLook)
+	fmt.Scan(&lookForMe)
+	findIt(lookForMe, whereToLook)
 	fmt.Println(whereToLook)
 }
+
+/*Задание 2. Нахождение первого вхождения числа в упорядоченном массиве (числа могут повторяться)
+Что нужно сделать
+Заполните упорядоченный массив из 12 элементов и введите число. Необходимо реализовать поиск первого вхождения заданного числа в массив. Сложность алгоритма должна быть минимальная.
+
+Что оценивается
+Верность индекса.
+
+При вводе массива 1 2 2 2 3 4 5 6 7 8 9 10 и вводе числа 2 программа должна вывести индекс 1.*/
