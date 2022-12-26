6b398301907025217897c0d17132f1822c5d5bdd
Vladimir <barakovvm@ya.ru>
2022-12-26 22:06:10 +0300

    25 done

---
+++ Go/25.Pointers/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+var Found bool = false
+
+func lookForSubString(str string, sstr string) {
+
+	sRune := []rune(str)
+	ssRune := []rune(sstr)
+	for i, v := range sRune {
+		if v == ssRune[0] {
+			for j := 1; j < len(ssRune); j++ {
+				if sRune[i+j] == ssRune[j] {
+					if j == len(ssRune)-1 {
+						Found = true
+						break
+					}
+				}
+			}
+
+		}
+
+	}
+	fmt.Println(Found)
+}
+
+func main() {
+	var whereToLook string
+	var whatToLook string
+
+	flag.StringVar(&whereToLook, "str", "", "set string")
+	flag.StringVar(&whatToLook, "substr", "", "set substring")
+	flag.Parse()
+	lookForSubString(whereToLook, whatToLook)
+
+}
+
+/*25.9 Практическая работа
+Цель задания
+Написать программу для нахождения подстроки в кириллической подстроке.
+Программа должна запускаться с помощью команды:
+
+ go run main.go --str "строка для поиска" --substr "поиска"
+
+Для реализации такой работы с флагами воспользуйтесь пакетом flags,
+а для поиска подстроки в строке вам понадобятся руны.
+
+Что нужно сделать
+Спроектировать алгоритм поиска подстроки.
+Определить строку и подстроку, используя флаги.
+Написать алгоритм реализацию для работы со строками UTF-8
+(для этого необходимо воспользоваться рунами).*/
---
+++ Go/25.Pointers/test.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func mainTest() {
+	fmt.Println(" ЭЭЭ\u001b[33m ЫЫыыы \u001b[0m dfghfgfg")
+}
